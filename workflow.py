bdcc96b80170a336f454abb37eae5ca0c7d22e8c
Yaron Haviv <yhaviv@gmail.com>
2020-02-20 17:51:32 +0000

    first push

--- workflow.py
+++ workflow.py
@@ -12,11 +12,9 @@ def init_functions(functions: dict, params=None, secrets=None):
 )
 def kfpipeline(p1=3):
     # first step build the function container
-    #builder = funcs['tstfunc'].deploy_step(with_mlrun=False)
+    builder = funcs['tstfunc'].deploy_step(with_mlrun=False)
 
     # first step
     s1 = funcs['tstfunc'].as_step(name='step-one', handler='my_func',
-         image='mlrun/mlrun:dev', #builder.outputs['image'],
+         image=builder.outputs['image'],
          params={'p1': p1}, out_path=artifacts_path)
-
-    # todo: 2nd step
